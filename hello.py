2bba6e2eb37d6be1c0ab4ff75119aebea295085d
Vaibhav Surase <vaibhavsurase@example.com>
2025-09-02 05:48:00 +0000

    Resolve conflict and keep both messages in hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 print("Hello, Jenkins! This is a test run.")
-=======
 print("Hello, world! from vaibhav surase this is webhook trigger jeb ngrok hello its vaibhav")
 print("hey its trigger automatically using webhook")
->>>>>>> 6133018 (Updated hello.py with webhook trigger print statements)
+
